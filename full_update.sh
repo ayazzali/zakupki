9f38850f964d9eb28e83395f6736c500bec9a4a8
Leo Schwartz <leo.schwartz@icloud.com>
2013-08-26 15:26:34 +0400

    Now full_update.sh logs errors.

--- full_update.sh
+++ full_update.sh
@@ -1,1 +1,1 @@
-python3 /home/roveo/data/zakupki/py/full_update.py >> /home/roveo/data/zakupki/logs/full_update.log
+python3 /home/roveo/data/zakupki/py/full_update.py &>> /home/roveo/data/zakupki/logs/full_update.log
