d30aa0ac7f3139d43b9b4d8ea949c93dc45315c9
Leo Schwartz <leo.schwartz@icloud.com>
2013-08-27 19:55:12 +0400

    full_update.sh to working mode

--- full_update.sh
+++ full_update.sh
@@ -1,2 +1,2 @@
-python3 ~/dev/zakupki/py/update.py full &>>~/dev/zakupki/logs/full_update.log
-python3 ~/dev/zakupki/py/update.py daily &>>~/dev/zakupki/logs/full_update.log
+python3 ~/data/zakupki/py/update.py full &>>~/data/zakupki/logs/full_update.log
+python3 ~/data/zakupki/py/update.py daily &>>~/data/zakupki/logs/full_update.log
