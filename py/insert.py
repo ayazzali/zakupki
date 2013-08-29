3c310fabfad1ac0a779cb9f5a6854d14bd27433c
Leo Schwartz <leo.schwartz@icloud.com>
2013-08-29 16:51:45 +0400

    1. Heavy code refactoring. 2. parameters full -> all, daily -> inc

---
+++ py/insert.py
@@ -0,0 +1,41 @@
+from utils import *
+from parse import *
+
+def insert_notifications(file_names, db, ftp, region):
+	for file_name in file_names:
+		print(ts(), file_name)
+		try:	
+			zip_file = retr(ftp, file_name, retry=5)
+			xml_file = unzip(zip_file)
+		except KeyboardInterrupt:
+			traceback.print_exc()
+			exit()
+		except AttributeError:
+			traceback.print_exc()
+			exit()
+		except:
+			traceback.print_exc()
+			continue
+		rows = []
+		for event, xml in etree.iterparse(xml_file, tag='{http://zakupki.gov.ru/oos/export/1}*'):
+			if event == 'end' and xml.tag != '{http://zakupki.gov.ru/oos/export/1}export':
+				rows.append(parse_notification(xml) + (region,))
+				xml.clear()
+		zip_file.close()
+		xml_file.close()
+		if len(rows) > 0:
+			try:
+				cur = db.cursor()
+				query = cur.mogrify('insert into notifications (rec_id, notification_number, notification_type, version_number, create_date, publish_date, placer_regnum, placer_name, order_name, last_name, first_name, middle_name, post_address, email, phone, href, print_form, max_price, folder_name)\nvalues ' + ',\n'.join(['%s'] * len(rows)), rows)
+				cur.execute(query)
+				db.commit()
+				cur.close()
+			except KeyboardInterrupt:
+				traceback.print_exc()
+				exit()
+			except AttributeError:
+				traceback.print_exc()
+				exit()
+			except:
+				traceback.print_exc()
+				continue
--- py/update.py
+++ py/update.py
@@ -1,68 +1,38 @@
 import argparse
 from ftplib import FTP
 import re
-import psycopg2 as db
+import psycopg2
 from lxml import etree
 from datetime import datetime, timedelta
 import gc
 
-from file_utils import * # ts(), retr(), unzip()
-from parse import *      # notification()
+from process import *
+
+config = {
+	'notifications': {
+		'ftp': FTP('ftp.zakupki.gov.ru', 'free', 'free'),
+		'db':  psycopg2.connect(database='zakupki', user='roveo'),
+		'process': process_notifications
+	},
+	'organization': {
+		'ftp': FTP('ftp.zakupki.gov.ru', 'anonymous'),
+		'db':  psycopg2.connect(database='zakupki', user='roveo'),
+		'process': lambda x: str(x)+'123'
+	}
+}
 
 if __name__ == '__main__':
 	# command line arguments
 	argparser = argparse.ArgumentParser()
-	argparser.add_argument(dest='type', choices=('daily', 'full'))
-	argparser.add_argument('-n', '--notifications', dest='scope', action='append_const', const='notifications')
+	argparser.add_argument(dest='type', choices=('inc', 'all')) # incremental or full update
+	argparser.add_argument('-n', '--notifications', dest='sections', action='append_const', const='notifications')
+	argparser.add_argument('-o', '--organization', dest='sections', action='append_const', const='organizations')
 	args = argparser.parse_args()
-	if not args.scope: args.scope = ['notifications']
+	if not args.sections: args.sections = ['notifications', 'organizations']
 
-	# connect FTP
-	print(ts(), 'connecting FTP...', end='\t')
-	zakupki_ftp = FTP('ftp.zakupki.gov.ru', 'free', 'free')
-	print('[DONE]')
-
-	# connect postgresql database
-	print(ts(), 'Connecting database zakupki...', end='\t')
-	zakupki_db = db.connect(database='zakupki', user='roveo')
-	zakupki_cur = zakupki_db.cursor()
-	print('[DONE]')
-
-	if args.type == 'full':
-		# truncate tables and drop indices
-		print(ts(), 'Truncating...', end='\t')
-		zakupki_cur.execute('truncate table notifications;')
-		zakupki_db.commit()
-		print('[DONE]')
-
-	# NLST folders
-	re_file = re.compile('.*\..*') # filter folders
-	folders = (name for name in zakupki_ftp.nlst() if not re_file.match(name))
-
-	ns = {'d': 'http://zakupki.gov.ru/oos/export/1', 's': 'http://zakupki.gov.ru/oos/types/1'} # XML namespace
-	# ['OK', 'EF', 'ZK', 'PO', 'SZ', 'Cancel']
-
-	for scope in args.scope:
-		for region in folders:
-			zakupki_ftp.cwd('/{region}/{scope}'.format(region=region, scope=scope)) # change wd
-			file_names = []
-			if args.type == 'daily':
-				zakupki_ftp.cwd('daily')
-				zakupki_cur.execute('select max(publish_date) from notifications where folder_name = %s;', (region,))
-				last_date = zakupki_cur.fetchone()[0]
-				if last_date:
-					current_date = last_date + timedelta(days=1)
-				else:
-					current_date = datetime.today() - timedelta(days=30)
-				end_date = datetime.today()
-				while current_date <= end_date: # from last date in this region to today
-					mask = current_date.strftime('*%Y%m%d_000000_') + (current_date + timedelta(days=1)).strftime('%Y%m%d_000000*.xml.zip')
-					file_names.extend(zakupki_ftp.nlst(mask))
-					current_date += timedelta(days=1)
-			elif args.type == 'full':
-				file_names.extend(zakupki_ftp.nlst('*.zip'))
-			insert_notifications(file_names, zakupki_db, zakupki_ftp, ns, region)
-
-	zakupki_ftp.close()
-	zakupki_cur.close()
-	zakupki_db.close()
+	for section in args.sections:
+		db = config[section]['db']
+		ftp = config[section]['ftp']
+		config[section]['process'](ftp, db, args.type)
+		db.close()
+		ftp.close()
