40649b06681872284791ff9d276ec2b027d00961
Leo Schwartz <leo.schwartz@icloud.com>
2013-09-09 18:13:35 +0400

    1. Added and fixed .sh for updates.

--- update_inc.sh
+++ update_inc.sh
@@ -1,5 +1,4 @@
 START=$(date +'%Y-%m-%d %H:%M:%S')
-# python /home/roveo/py/update.py inc
+python /home/roveo/data/zakupki/py/update.py all -p -c 2>&1 | tee -a /home/roveo/data/zakupki/log/inc.log | tee /home/roveo/data/zakupki/log/email_inc.log
 END=$(date +'%Y-%m-%d %H:%M:%S')
-echo 
-sendemail -f leo.schwartz@icloud.com -t leo.schwartz@icloud.com -u "Incremental update: zakupki" -m "Incremental update started $START, ended $END (zakupki)." -s smtp.mail.me.com:587 -xu leo.schwartz@me.com -xp Triest15 -o tls=yes
+sendemail -f leo.schwartz@icloud.com -t leo.schwartz@icloud.com -u "Incremental update: zakupki" -m "Incremental update started $START, ended $END (zakupki)." -a /home/roveo/data/zakupki/log/email_inc.log -s smtp.mail.me.com:587 -xu leo.schwartz@me.com -xp Triest15 -o tls=yes
