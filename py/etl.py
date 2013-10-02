29a7d80e0fc571333c465182e25cb8fe3996b767
Leo Schwartz <leo.schwartz@icloud.com>
2013-10-02 18:14:50 +0400

    resolving conflict

--- py/etl.py
+++ py/etl.py
@@ -20,9 +20,7 @@ def contracts_etl(ftp, collection, update_type):
 	total_size = 0.0
 	for region in folders:
 		region_files = inc_files(collection, ftp, region) if update_type == 'inc' else all_files(collection, ftp, region)
-		size = 0.0
-		for f in region_files:
-			size += ftp.size(f)
+		size = ftp_size(ftp, region_files)
 		files.extend([(f, region) for f in region_files])
 		print ts(), region, round(size / (1024 * 1024), 2), 'Mb'
 		total_size += size
@@ -48,9 +46,7 @@ def products_etl(ftp, collection, update_type):
 		mask = 'nsiProduct_inc_*.xml.zip'
 	ftp.cwd('/auto/product/')
 	files = nlst(ftp, mask)
-	size = 0.0
-	for f in files:
-		size += ftp.size(f)
+	size = ftp_size(ftp, files)
 	print 'Loading {len} files, {size} Mb total.'.format(len=len(files), size=round(size / (1024 * 1024), 2))
 	for f in files:
 		print ts(), f
@@ -61,6 +57,27 @@ def products_etl(ftp, collection, update_type):
 					document = transform_product(xml)
 					if document: # products document can be None if code is a string (it happens)
 						load(collection, document, upsert=True)
+					xml.clear()
+		else:
+			print ts(), 'Error loading file!'
+
+def organizations_etl(ftp, collection, update_type):
+	if update_type == 'all':
+		mask = '*.xml.zip'	
+	elif update_type == 'inc':
+		mask = 'organization_inc*.xml.zip'
+	ftp.cwd('/auto/organization/all')
+	files = nlst(ftp, mask)
+	size = ftp_size(ftp, files)
+	print 'Loading {len} files, {size} Mb total.'.format(len=len(files), size=round(size / (1024 * 1024), 2))
+	for f in files:
+		print ts(), f
+		xml_file = extract(ftp, f)
+		if xml_file:
+			for event, xml in etree.iterparse(xml_file, tag='{http://zakupki.gov.ru/oos/types/1}organization'):
+				if event == 'end':
+					document = transform_organization(xml)
+					load(collection, document, upsert=True)
 					xml.clear()
 		else:
 			print ts(), 'Error loading file!'
