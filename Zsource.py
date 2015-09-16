b9fd76a540cac681034a5837606d00063f8c2a08
Mikhail.Akimov <mikhail.akimov@inn.ru>
2015-09-16 18:24:22 +0300

    Changes in Zsource

--- Zsource.py
+++ Zsource.py
@@ -3,51 +3,57 @@ from ftplib import FTP
 from tempfile import TemporaryFile
 from zipfile import ZipFile, ZipInfo
 
+from time import sleep
+
+
 class Zsource:
 
     ''' This class represents an interface to Zakupki's data source.
-        Use to retrieve raw XML data applying arbitrary filters.
+
+        Use to retrieve file lists and then raw XML files applying
+        arbitrary filters.
     '''
 
     def __init__(self):
-        ''' init
+        ''' Initialize connection to Zakupki FTP.
         '''
         self.connection = FTP('ftp.zakupki.gov.ru', user='free', passwd='free')
         self.connection.cwd('fcs_regions')
 
-    def refresh_list(self, region=''):
-        ''' refresh_list
+    def refresh_list(self, regions=[]):
+        ''' Refresh list of files for a given list of regions.
         '''
         self.files = []
-        file_re = re.compile(r'^.*\.\w{,4}$')
+        file_re = re.compile(r'^.*\.\w{,3}$')
         folders = []
-        if region and region not in self.list_regions():
-            print('Region {} not found, loading all regions'.format(region))
-        current_folder = region
-        while True:
-            print(' ' * 100, end='\r')
-            print(current_folder, len(self.files), sep='\t')
-            nlst = self.connection.nlst(current_folder)
-            folders.extend([x for x in nlst if not file_re.match(x) and x != current_folder])
-            if len(folders) == 0:
-                break
-            current_folder = folders.pop(0)
-            self.files.extend([x for x in nlst if file_re.match(x)])
+        all_regions = self.list_regions()
+        for current_folder in regions:
+            while True:
+                print(' ' * 100, end='\r')
+                print(current_folder, len(self.files), sep='\t')
+                nlst = self.connection.nlst(current_folder)
+                folders.extend([x for x in nlst if not file_re.match(x) and x != current_folder])
+                if len(folders) == 0:
+                    break
+                current_folder = folders.pop(0)
+                self.files.extend([x for x in nlst if file_re.match(x)])
 
     def list_regions(self):
-        ''' list_regions
+        ''' Return a list of available region directories.
         '''
         return [x for x in self.connection.nlst() if not x.startswith('_')]
 
     def list_document_types(self):
-        ''' list_document_types
+        ''' Return a list of document types in the current self.files list.
         '''
         result = set()
         type_re = re.compile(r'.*/(\w+?)_[^/]+$')
         return set(type_re.match(f).group(1) for f in self.files)
 
-    def retr_files(self, region=None, document_type=None):
-        ''' retr_files
+    def get_files(self, region=None, document_type=None):
+        ''' Generator that returns file handles for raw XML (unzipped) files
+            from self.files list. Additionally can be filtered by document_type
+            and region.
         '''
         files = self.files
         if region and region in self.list_regions():
@@ -55,10 +61,14 @@ class Zsource:
         if document_type and document_type in self.list_document_types():
             files = [f for f in files if document_type in f]
         for path in files:
-            for file in self.retr_file(path):
-                yield file
+            for f in self.retr_file(path):
+                yield f
+            sleep(1)
 
     def retr_file(self, path, retry=3):
+        ''' Gererator that returns file handles for raw XML (unzipped) files
+            inside a ZIP file at a given FTP path.
+        '''
         ftp_size = self.connection.size(path)
         while retry > 0:
             tmp = TemporaryFile()
@@ -71,3 +81,8 @@ class Zsource:
             for i in zf.infolist():
                 yield zf.open(i)
         tmp.close()
+
+    def download_files(self, dir='.', unzip=True):
+        ''' download_files
+            TO-DO: Save raw XML (or ZIP) files into a directory.
+        '''
--- __init__.py
+++ __init__.py
@@ -1,82 +1,5 @@
-from lxml import etree
 import codecs
-import json
-import collections
-from ftplib import FTP
-import re
 from time import sleep
-from tempfile import TemporaryFile
-from zipfile import ZipFile, ZipInfo
-
-
-class Zsource:
-
-    ''' This class represents an interface to Zakupki's data source.
-        Use to retrieve raw XML data applying arbitrary filters.
-    '''
-
-    def __init__(self):
-        ''' init
-        '''
-        self.connection = FTP('ftp.zakupki.gov.ru', user='free', passwd='free')
-        self.connection.cwd('fcs_regions')
-
-    def refresh_list(self, region=''):
-        ''' refresh_list
-        '''
-        self.files = []
-        file_re = re.compile(r'^.*\.\w{,4}$')
-        folders = []
-        if region and region not in self.list_regions():
-            print('Region {} not found, loading all regions'.format(region))
-        current_folder = region
-        while True:
-            print(' ' * 100, end='\r')
-            print(current_folder, len(self.files), sep='\t')
-            nlst = self.connection.nlst(current_folder)
-            folders.extend([x for x in nlst if not file_re.match(x) and x != current_folder])
-            if len(folders) == 0:
-                break
-            current_folder = folders.pop(0)
-            self.files.extend([x for x in nlst if file_re.match(x)])
-
-    def list_regions(self):
-        ''' list_regions
-        '''
-        return [x for x in self.connection.nlst() if not x.startswith('_')]
-
-    def list_document_types(self):
-        ''' list_document_types
-        '''
-        result = set()
-        type_re = re.compile(r'.*/(\w+?)_[^/]+$')
-        return set(type_re.match(f).group(1) for f in self.files)
-
-    def retr_files(self, region=None, document_type=None):
-        ''' retr_files
-        '''
-        files = self.files
-        if region and region in self.list_regions():
-            files = [f for f in files if f.startswith(region)]
-        if document_type and document_type in self.list_document_types():
-            files = [f for f in files if document_type in f]
-        for path in files:
-            for file in self.retr_file(path):
-                yield file
-
-    def retr_file(self, path, retry=3):
-        ftp_size = self.connection.size(path)
-        while retry > 0:
-            tmp = TemporaryFile()
-            self.connection.retrbinary('RETR {}'.format(path), tmp.write)
-            down_size = tmp.tell()
-            if down_size == ftp_size:
-                break
-            retry -= 1
-        with ZipFile(tmp) as zf:
-            for i in zf.infolist():
-                yield zf.open(i)
-        tmp.close()
 
 
 def get_element_path(element):
@@ -103,10 +26,3 @@ def get_element_dict(element):
                 result[name] = []
             result[name].append(get_element_dict(child))
     return result
-
-
-# with codecs.open('test.json', 'w', encoding='UTF-8') as output:
-#     for event, element in xml:
-#         json.dump(get_element_dict(element), output, ensure_ascii=False)
-#         output.write('\n')
-#         element.clear()
